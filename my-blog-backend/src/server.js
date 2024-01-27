19f4dbac2563b639b0310defc017d08979be7943
Ishaan1103 <ishaanrana2635@gmail.com>
2024-01-27 16:50:31 +0530

    Mongo-Db_added

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { db,connectToDb } from './db.js';
 let articleInfo=[{
     name:'learn-node',
     upvote: 0,
@@ -8,17 +9,37 @@ let articleInfo=[{
     upvote:0,
     comments:[],
 },{
-    name:'mongo',
+    name:'my-thoughts-on-resumes',
     upvote:0,
     comments:[],
 }]
 
 const app = express();
 app.use(express.json());
+app.get('/api/articles/:name', async (req,res)=>{
+const {name}=req.params;
 
-app.put('/api/articles/:name/upvote',(req,res)=>{
+
+
+const article =await db.collection('articles').findOne({name});
+
+if(article){
+res.json(article);
+}
+else{
+res.sendStatus(404);
+}
+});
+app.put('/api/articles/:name/upvote',async (req,res)=>{
     const {name}=req.params;
-    const article=articleInfo.find(a=> a.name === name);
+    
+    await db.collection('artice').updateOne({ name }, {
+        $inc:{ upvotes: 1 },
+    });
+    
+    const article =await db.collection('articles').findOne({name});
+    
+    
     if(article){
         article.upvote+=1;
         res.send(`The ${name} artticle now has ${article.upvote} upvote!!!`);
@@ -28,21 +49,25 @@ app.put('/api/articles/:name/upvote',(req,res)=>{
     }
 });
 
-app.post('/api/article/:name/comments',(req,res)=>{
+app.post('/api/articles/:name/comments',async(req,res)=>{
     const {name}=req.params;
     const {postedBy,text}=req.body;
 
-
-    const article =articleInfo.find(a=>a.name === name);
+   
+    await db.collection('articles').updateOne({name},{
+        $push:{comments:{postedBy,text}},
+    });
+    const article = await db.collection('articles').findOne({name});
     if(article){
-    article.comments.push({postedBy,text});
     res.send(article.comments);
 }
 else{
     res.send('article doesn\'t exist');
 }
 });
-
-app.listen(8000,()=>{
-    console.log('server is listening on port 8000');
+connectToDb(()=>{
+    console.log('Connected to database');
+    app.listen(8000,()=>{
+        console.log('server is listening on port 8000');
+    });
 });
